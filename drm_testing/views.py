2c30cdb0da5e58fb3dcd3eac194aa314968fde9e
hari-testpress <hari@testpress.in>
2024-02-29 10:05:24 +0530

    Modify settings

--- drm_testing/views.py
+++ drm_testing/views.py
@@ -1,13 +1,13 @@
-import json
 import base64
+import json
 
 import requests
 from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import status
-
 from rest_framework.parsers import BaseParser
 from rest_framework.views import APIView
+
 
 class OctetStreamParser(BaseParser):
     charset = None
@@ -26,17 +26,20 @@ class DRMProxyView(APIView):
         asset_id = kwargs.get('asset_id')
         access_token = self.request.query_params.get("access_token", "")
         body = {
-            "player_payload":  base64.b64encode(self.request.data).decode('utf-8'),
+            "player_payload": base64.b64encode(self.request.data).decode('utf-8'),
             "license_specs": {
-                "security_level": 1,
-                "required_output_protection": {
-                    "hdcp": "HDCP_V1"
-                }
+                "content_key_specs": [
+                    {'track_type': 'SD', 'security_level': 1, 'required_output_protection': {'hdcp': 'HDCP_V1'}},
+                    {'track_type': 'HD', 'security_level': 1, 'required_output_protection': {'hdcp': 'HDCP_V1'}},
+                    {'track_type': 'UHD1', 'security_level': 1, 'required_output_protection': {'hdcp': 'HDCP_V1'}},
+                    {'track_type': 'UHD2', 'security_level': 1, 'required_output_protection': {'hdcp': 'HDCP_V1'}},
+                    {'track_type': 'AUDIO', 'security_level': 1, 'required_output_protection': {'hdcp': 'HDCP_V1'}}],
             }
         }
-        license_url = f"https://8bde-49-43-249-218.ngrok-free.app/api/v1/k6gdyc/assets/{asset_id}/drm_license/?access_token={access_token}"
+        license_url = f"https://35ce-183-82-205-19.ngrok-free.app/api/v1/k6gdyc/assets/{asset_id}/drm_license/?access_token={access_token}"
 
-        license_response = requests.post(license_url, data=json.dumps(body), headers={"content-type": "application/json"} )
+        license_response = requests.post(license_url, data=json.dumps(body),
+                                         headers={"content-type": "application/json"})
         license_data = license_response.content
         license_response.raise_for_status()
 
