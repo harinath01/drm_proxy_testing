4772e152a00fd9930730129e1da2b67ad1950e1d
hari-testpress <hari@testpress.in>
2024-03-11 12:11:35 +0530

    Make DRM proxy API work for Fairplay License request

--- drm_testing/views.py
+++ drm_testing/views.py
@@ -25,9 +25,11 @@ class DRMProxyView(APIView):
     def post(self, request, *args, **kwargs):
         asset_id = kwargs.get('asset_id')
         access_token = self.request.query_params.get("access_token", "")
+        drm_type = self.request.query_params.get("drm_type", "")
+
         body = {
             "player_payload": base64.b64encode(self.request.data).decode('utf-8'),
-            "license_specs": {
+            "widevine": {
                 "content_key_specs": [
                     {'track_type': 'SD', 'security_level': 1, 'required_output_protection': {'hdcp': 'HDCP_V1'}},
                     {'track_type': 'HD', 'security_level': 1, 'required_output_protection': {'hdcp': 'HDCP_V1'}},
@@ -36,7 +38,7 @@ class DRMProxyView(APIView):
                     {'track_type': 'AUDIO', 'security_level': 1, 'required_output_protection': {'hdcp': 'HDCP_V1'}}],
             }
         }
-        license_url = f"https://app.tpstreams.com/api/v1/6eafqn/assets/{asset_id}/drm_license/?access_token={access_token}"
+        license_url = f"https://app.tpstreams.com/api/v1/6eafqn/assets/{asset_id}/drm_license/?access_token={access_token}&drm_type={drm_type}"
 
         license_response = requests.post(license_url, data=json.dumps(body),
                                          headers={"content-type": "application/json"})
